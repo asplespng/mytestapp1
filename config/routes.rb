ad0c0ee9130ce14bdbb82a21490279d39adceb17
asplespng <per.evensen@gmail.com>
2015-09-03 00:12:38 +0000

    various changes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
+  resources :users
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
