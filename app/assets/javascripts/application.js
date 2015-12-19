c3d34b3b03e89d9d93148d9289ad8da7b3ea187d
Per Evensen <per.evensen@gmail.com>
2015-12-18 22:25:46 -0500

    query bootgrid

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require jquery_ujs
-//= require turbolinks
 //= require_tree .
 //= require bootstrap-sprockets
 //= require jquery.bootgrid
